667f202d28c2df69e272bd31d47a0ff0452504f2
OlesjaD <113940197+OlesjaD@users.noreply.github.com>
2023-03-12 22:28:39 +0000

    Deploying to gh-pages from @ OlesjaD/goit-js-hw-09@1ea482c879cec86a4981668e5caa49524dcb0b77 🚀

---
+++ 01-color-switcher.25906ac5.js
@@ -0,0 +1,2 @@
+document.querySelector("[data-start]");const t=document.querySelector("[data-stop]");console.log(t);
+//# sourceMappingURL=01-color-switcher.25906ac5.js.map
---
+++ 01-color-switcher.e9ad00c5.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector("[data-start]");var t=document.querySelector("[data-stop]");console.log(t)}();
+//# sourceMappingURL=01-color-switcher.e9ad00c5.js.map
