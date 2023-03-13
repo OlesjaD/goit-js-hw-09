8f068f3c95cb71d72ae3edb29572f8203fc28fcc
OlesjaD <olesja.d.c@gmail.com>
2023-03-13 23:40:36 +0200

    task01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,25 @@
-const btnStart = document.querySelector('[data-start]');
-const btnStop = document.querySelector('[data-stop]');
-console.log(btnStop);
+import '../css/common.css';
+
+const refs = {
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]'),
+};
+
+let timerId = null;
+
+refs.btnStart.addEventListener('click', () => {
+    refs.btnStart.disabled = true;
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
+  
+refs.btnStop.addEventListener("click", () => {
+    clearInterval(timerId);
+    refs.btnStart.disabled = false;
+});
+    
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
